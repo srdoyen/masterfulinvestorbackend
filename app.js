69beb558ec109820b66d5faa55bf88df8c2d50ec
Scott Doyen <srdoyen@gmail.com>
2020-07-10 17:34:41 -0700

    rest api w express ad mongoose

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ var campsiteRouter = require("./routes/campsiteRouter");
 var partnerRouter = require("./routes/partnerRouter");
 var promotionRouter = require("./routes/promotionRouter");
 
+const mongoose = require("mongoose");
+
+const url = "mongodb://localhost:27017/nucampsite";
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connect.then(
+  () => console.log("Connected correctly to server"),
+  (err) => console.log(err)
+);
+
 var app = express();
 
 // view engine setup
