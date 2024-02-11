e7d78c5f17dde63ea3f6fa1c6dc9928eb1dc4aa2
Scott Doyen <srdoyen@gmail.com>
2024-02-10 20:55:46 -0800

    using .env

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
+
 module.exports = {
-  secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/masterfulinvestor",
+  secretKey: process.env.SECRET_KEY,
+  mongoUrl: process.env.MONGO_URL,
 };
