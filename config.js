54fd0dc61f070b3866448bc3752442799b8c68bf
Scott Doyen <srdoyen@gmail.com>
2024-02-10 21:12:59 -0800

    Added secret key and mongo

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 const result = require("dotenv").config();
-console.log(result); // Check if any error is reported
-console.log(process.env); // Should include your .env variables
 
 module.exports = {
   secretKey: "12345-67890-09876-54321",
